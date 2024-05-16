aa301f903ad39c7290cd6aa7ece795d1dd8573d2
jairaleg <jairaleg@gmail.com>
2024-05-16 16:07:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
