67632438c6d823ddd74221fc25ed990dc034fa22
jairaleg <jairaleg@gmail.com>
2024-05-16 16:22:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 
 # Write directly to the app
@@ -31,6 +31,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
     #st.write(ingredients_string)
     
@@ -46,7 +48,3 @@ if ingredients_list:
 
         
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json(),user_container_width=True)
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
